61e6225b81cd06b301608ba5c118efa39e691ef9
Eduardo Ribeiro <eduardo20003131@gamil.com>
2023-05-27 19:10:22 -0300

    Exercicio 01

---
+++ Logica/Logica/Exercicios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    static class Exercicios
+    {
+    public static List<string> Exercicio_01(int n){
+
+            List<string> lista = new List<string>();
+
+            for(int i = 1; i <= n; i++) {
+                string asterico = new string('*', i);
+                lista.Add(asterico);
+            }
+
+            string listaCompleta = "[" + string.Join(",", lista) + "]";
+            Console.WriteLine(listaCompleta);
+
+            return lista;
+        }
+    }
+
+  
+}
---
+++ Logica/Logica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Exercicios.Exercicio_01(5);
+            
+           
+        }
+    }
+}
