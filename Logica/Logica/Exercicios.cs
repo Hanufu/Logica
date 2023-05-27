fcb4b4c69de700b68100798f9febe6d7c3ed2764
Eduardo Ribeiro <eduardo20003131@gamil.com>
2023-05-27 19:40:09 -0300

    Exercicio 2

--- Logica/Logica/Exercicios.cs
+++ Logica/Logica/Exercicios.cs
@@ -10,21 +10,54 @@ namespace Logica
 {
     static class Exercicios
     {
-    public static List<string> Exercicio_01(int n){
+        public static List<string> Exercicio_01(int n)
+        {
 
             List<string> lista = new List<string>();
 
-            for(int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n; i++)
+            {
                 string asterico = new string('*', i);
                 lista.Add(asterico);
             }
 
+            //mostra resultado
             string listaCompleta = "[" + string.Join(",", lista) + "]";
             Console.WriteLine(listaCompleta);
 
             return lista;
         }
-    }
 
-  
+        public static List<(int, int)> Exercicio_02(int[] array)
+        {
+            
+            Array.Sort(array);
+            int menorDiferenca = int.MaxValue;
+
+            //Encontra a menor diferença absoluta
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int diferenca = Math.Abs(array[i] - array[i + 1]);
+                menorDiferenca = Math.Min(menorDiferenca, diferenca);
+            }
+
+            //Encontra todos os pares com a menor diferenlça
+            List<(int, int)> pares = new List<(int, int)>();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int diferenca = Math.Abs(array[i] - array[i + 1]);
+                if (diferenca == menorDiferenca)
+                    pares.Add((array[i], array[i + 1]));
+            }
+
+            //mostra resultado
+            List<(int, int)> resultado = pares;
+            foreach (var par in resultado)
+            {
+                Console.WriteLine($"({par.Item1}, {par.Item2})");
+            }
+            return pares;
+        }
+
+    }
 }
--- Logica/Logica/Program.cs
+++ Logica/Logica/Program.cs
@@ -6,9 +6,12 @@ namespace Logica
     {
         static void Main(string[] args)
         {
-          Exercicios.Exercicio_01(5);
-            
-           
+            //Exercicios.Exercicio_01(5);
+
+
+            int[] array = { 3, 8, 50, 5, 1, 18, 121, 122, 125, 131, 133, 34, 51, 41, 40 };
+            Exercicios.Exercicio_02(array); 
+         
         }
     }
 }
