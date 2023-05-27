adb605c43e177262e5ef26fa76a4daccc7cf1bc0
Eduardo Ribeiro <eduardo20003131@gamil.com>
2023-05-27 20:10:55 -0300

    Ajuste de pastas e apresentação do programa principal main

---
+++ TesteDeLogica/TesteDeLogica/Exercicios.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeLogica
+{
+    static class Exercicios
+    {
+        public static List<string> Exercicio_01(int n)
+        {
+
+            List<string> lista = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string asterico = new string('*', i);
+                lista.Add(asterico);
+            }
+
+            //mostra resultado
+            string listaCompleta = "[" + string.Join(",", lista) + "]";
+            Console.WriteLine(listaCompleta);
+
+            return lista;
+        }
+
+        public static List<(int, int)> Exercicio_02(int[] array)
+        {
+
+            Array.Sort(array);
+            int menorDiferenca = int.MaxValue;
+
+            //Encontra a menor diferença absoluta
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int diferenca = Math.Abs(array[i] - array[i + 1]);
+                menorDiferenca = Math.Min(menorDiferenca, diferenca);
+            }
+
+            //Encontra todos os pares com a menor diferenlça
+            List<(int, int)> pares = new List<(int, int)>();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int diferenca = Math.Abs(array[i] - array[i + 1]);
+                if (diferenca == menorDiferenca)
+                    pares.Add((array[i], array[i + 1]));
+            }
+
+            //mostra resultado
+            List<(int, int)> resultado = pares;
+            foreach (var par in resultado)
+            {
+                Console.WriteLine($"({par.Item1}, {par.Item2})");
+            }
+            return pares;
+        }
+
+        public static List<List<int>> Exercicio_03(int[] array)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            //Começa com um sobconjunto vazio
+            result.Add(new List<int>());
+
+            foreach (int item in array)
+            {
+                int count = result.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    List<int> subset = new List<int>(result[i]);
+                    subset.Add(item);
+                    result.Add(subset);
+                }
+            }
+
+            //Mostra resultado
+            List<List<int>> subsets = result;
+            foreach (List<int> subset in subsets)
+            {
+                Console.WriteLine("[{0}]", string.Join(", ", subset));
+            }
+            return result;
+        }
+    }
+}
+
---
+++ TesteDeLogica/TesteDeLogica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TesteDeLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio I\n");
+            Exercicios.Exercicio_01(5);
+            Console.WriteLine("\n---------------------------------");
+
+
+            Console.WriteLine("\nExercicio II\n");
+            int[] array = { 3, 8, 50, 5, 1, 18, 121, 122, 125, 131, 133, 34, 51, 41, 40 };
+            Exercicios.Exercicio_02(array);
+            Console.WriteLine("\n---------------------------------");
+
+
+            Console.WriteLine("\nExercicio III\n");
+            int[] numeros = { 1, 2, 3 };
+            Exercicios.Exercicio_03(numeros);
+
+
+        }
+    }
+}
